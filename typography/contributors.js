19a7e0708e414c0ba111f1b7de13204cdbeb2aee
Harjot S <harjotkarwal1221@gmail.com>
2020-10-25 23:53:56 +0530

    rainbow by harjot

--- typography/contributors.js
+++ typography/contributors.js
@@ -2,4 +2,5 @@ window.animations = [
     { name: 'demo', author: 'anmolcool007', author_url: 'https://github.com/anmolcool007' },
     { name: 'neon-flikr', author: 'anmolcool007', author_url: 'https://github.com/anmolcool007' },
     { name: 'pulse', author: 'harjotkarwal', author_url: 'https://github.com/harjotkarwal' },
+    { name: 'rainbow', author: 'harjotkarwal', author_url: 'https://github.com/harjotkarwal' },
 ];
