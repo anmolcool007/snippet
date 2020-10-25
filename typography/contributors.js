8738241f65ef87cb642fbf185badf918de81bed1
Harjot S <harjotkarwal1221@gmail.com>
2020-10-25 18:56:03 +0530

    pulse by harjot

--- typography/contributors.js
+++ typography/contributors.js
@@ -1,4 +1,5 @@
 window.animations = [
     { name: 'demo', author: 'anmolcool007', author_url: 'https://github.com/anmolcool007' },
     { name: 'neon-flikr', author: 'anmolcool007', author_url: 'https://github.com/anmolcool007' },
+    { name: 'pulse', author: 'harjotkarwal', author_url: 'https://github.com/harjotkarwal' },
 ];
