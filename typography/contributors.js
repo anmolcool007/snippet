219fbbbd4a68f52d48277444c8571ee956a55593
Yash5044 <yashchaubey2001@gmail.com>
2020-10-26 19:35:04 +0530

    artistic typeface

--- typography/contributors.js
+++ typography/contributors.js
@@ -3,4 +3,5 @@ window.animations = [
     { name: 'neon-flikr', author: 'anmolcool007', author_url: 'https://github.com/anmolcool007' },
     { name: 'pulse', author: 'harjotkarwal', author_url: 'https://github.com/harjotkarwal' },
     { name: 'rainbow', author: 'harjotkarwal', author_url: 'https://github.com/harjotkarwal' },
+    { name: 'nirvana', author: 'yash5044', author_url: 'https://github.com/yash5044' },
 ];
